324acbef885d9185ecaf67901770f1bc7f86c30d
JureKotnik <jure2002k@gmail.com>
2026-01-05 21:41:36 +0100

    Initial commit: Spring Boot 4 setup

---
+++ src/main/java/io/github/jurekotnik/smartspend/SmartspendApplication.java
@@ -0,0 +1,13 @@
+package io.github.jurekotnik.smartspend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartspendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartspendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/jurekotnik/smartspend/SmartspendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.jurekotnik.smartspend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartspendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
