27ab1acfb32d34acff443079f76b2cb83a0579e2
JureKotnik <jure2002k@gmail.com>
2026-01-08 19:59:29 +0100

    add validation rules to transaction entity

--- src/main/java/io/github/jurekotnik/smartspend/controller/TransactionController.java
+++ src/main/java/io/github/jurekotnik/smartspend/controller/TransactionController.java
@@ -2,6 +2,8 @@ package io.github.jurekotnik.smartspend.controller;
 
 import io.github.jurekotnik.smartspend.entity.Transaction;
 import io.github.jurekotnik.smartspend.repository.TransactionRepository;
+import jakarta.validation.Valid;
+
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -22,7 +24,7 @@ public class TransactionController {
     }
 
     @PostMapping
-    public Transaction createTransaction(@RequestBody Transaction transaction) {
+    public Transaction createTransaction(@Valid@RequestBody Transaction transaction) {
         return repository.save(transaction);
     }
 }
--- src/main/java/io/github/jurekotnik/smartspend/entity/Transaction.java
+++ src/main/java/io/github/jurekotnik/smartspend/entity/Transaction.java
@@ -1,6 +1,7 @@
 package io.github.jurekotnik.smartspend.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
 import lombok.Data;
 import java.time.LocalDate;
 
@@ -10,15 +11,20 @@ import java.time.LocalDate;
 public class Transaction {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY) // Auto-increment
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false)
+    @NotBlank(message = "Description is required")
+    @Size(min = 3, message = "Description must be at least 3 chars")
     private String description;
 
     @Column(nullable = false)
+    @NotNull(message = "Amount is required")
+    @Positive(message = "Amount must be greater than zero")
     private Double amount;
 
+    @PastOrPresent(message = "Date cannot be in the future")
     private LocalDate date;
-
+    
 }
